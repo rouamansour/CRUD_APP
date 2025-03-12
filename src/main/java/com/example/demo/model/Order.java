5f8985c9872f9778370011db43958413a4b9fe58
roua.mansour <roua.mansour@proxym-it.com>
2025-03-13 00:11:46 +0100

    add annotation for entity order

--- src/main/java/com/example/demo/model/Order.java
+++ src/main/java/com/example/demo/model/Order.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,6 +18,7 @@ public class Order {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonIgnore
     private User user;
 
     public Order() {
