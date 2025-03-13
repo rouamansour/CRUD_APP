a02ffe833fa1cf89a75e4d5528905f06cd5d8f7c
roua.mansour <roua.mansour@proxym-it.com>
2025-03-13 01:27:16 +0100

    add file data.sql to insert data in database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO users (name, email) VALUES ('roua', 'roua@example.com');
+INSERT INTO users (name, email) VALUES ('belsem', 'bob@gmail.com');
+
+INSERT INTO orders (product, amount, user_id) VALUES ('Product 1', 100.50, 1);
+INSERT INTO orders (product, amount, user_id) VALUES ('Product 2', 200.0, 1);
+INSERT INTO orders (product, amount, user_id) VALUES ('Product 3', 100.0, 2);
